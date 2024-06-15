dbf75ae6fa96a38d0ae249b44bc85222c4bdfe75
Mo-Haru <furtive-rapture.0t@icloud.com>
2024-06-15 22:54:54 +0900

    a

--- testapp/views.py
+++ testapp/views.py
@@ -1,5 +1,6 @@
 from testapp import app, db
 from flask import render_template, request
+from testapp.models import reserve
 
 @app.route('/')
 def index():
@@ -21,6 +22,12 @@ def reserve():
         return render_template("testapp/reserve.html", webtitle = title)
     if request.method == "POST":
         buycnt = request.form["buycnt"]
-        
+        reserve = reserve(
+            reserver="a",
+            e_mail="a",
+            reserve_dic={"唐揚げ": buycnt}
+        )
+        db.session.add(reserve)
+        db.session.commit()
         title = "予約完了"
         return render_template("testapp/reserve-done.html", webtitle = title, buycnt=buycnt)
