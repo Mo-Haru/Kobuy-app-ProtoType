69d14f1df79f379b4f4faae1d1642ad43cadf426
kaede102526 <tkg2210304hi@fukui-ed.jp>
2024-06-16 23:04:29 +0900

    a

--- testapp/static/javascript/kari.js
+++ testapp/static/javascript/kari.js
@@ -1,1 +1,8 @@
-a
+const $wrap = document.querySelector('.number-spinner-wrap')
+const $input = $wrap.querySelector('input')
+$wrap.querySelector('.spinner-down').onclick = ()=>{
+  $input.stepDown()
+}
+$wrap.querySelector('.spinner-up').onclick = ()=>{
+  $input.stepUp()
+}
