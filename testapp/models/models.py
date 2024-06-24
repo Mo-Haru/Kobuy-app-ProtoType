f04abc2c4194970ad6bc1cb964625beb01e8afce
Mo-Haru <furtive-rapture.0t@icloud.com>
2024-06-24 21:58:01 +0900

    a

--- testapp/models/models.py
+++ testapp/models/models.py
@@ -1,5 +1,12 @@
 from testapp import db
+from flask_login import UserMixin
 from datetime import datetime
+
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, nullable=False)
+    password = db.Column(db.String(128), nullable=False)
 
 class Menu(db.Model):
     __tablename__ = 'menu'
@@ -18,3 +25,9 @@ class Reserve(db.Model):
     resreve_dic = db.Column(db.String(255))
     reserve_time = db.Column(db.DateTime, default = datetime.now)
     cancel = db.Column(db.Boolean)
+
+class Notification(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(128), nullable=False)
+    content = db.Column(db.Text, nullable=False)
+    date_posted = db.Column(db.DateTime, default=datetime.now)
